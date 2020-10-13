16a55a66dd308921c5ad6654dd413b4f8bab35a9
psai11 <saichaita@gmail.com>
2020-10-14 01:24:14 +0530

    On 14 oct

--- includes/classes/Message.php
+++ includes/classes/Message.php
@@ -54,7 +54,123 @@ class Message {
 		return $data;
 	}
 
+	public function getLatestMessage($userLoggedIn, $user2) {
+		$details_array = array();
 
+		$query = mysqli_query($this->con, "SELECT BODY,USER_TO,DATE FROM MESSAGES WHERE (USER_TO='$userLoggedIn' AND USER_FROM='$user2') OR (USER_TO='$user2' AND USER_FROM='$userLoggedIn') ORDER BY ID DESC LIMIT 1");
+
+		$row = mysqli_fetch_array($query);
+		$sent_by = ($row['USER_TO'] == $userLoggedIn) ? "They said: " : "You said: ";
+
+
+		//TimeFrame
+		$date_time_now = date("Y-m-d H:i:s");
+		$start_date = new DateTime($row['DATE']); // Time of post
+		$end_date = new DateTime($date_time_now); // Current time
+		$interval = $start_date->diff($end_date); // Difference between dates
+		if ($interval->y >= 1) {
+			if ($interval == 1)
+				$time_message = $interval->y . " year ago"; //1 year ago
+			else
+				$time_message = $interval->y . " years ago"; //1+ year ago
+		}
+		else if ($interval->m >= 1) {
+			if ($interval->d == 0) {
+				$days = " ago";
+			}
+			else if($interval->d == 1) {
+				$days = $interval->d . " day ago";
+			}
+			else {
+				$days = $interval->d . " days ago";
+			}
+
+			if ($interval->m == 1) {
+				$time_message = $interval->m . " month". $days;
+			}
+			else {
+				$time_message = $interval->m . " months". $days;
+			}
+
+		}
+		else if ($interval->d >= 1) {
+			if($interval->d == 1) {
+				$time_message = "Yesterday";
+			}
+			else {
+				$time_message = $interval->d . " days ago";
+			}
+		}
+		else if ($interval->h >= 1) {
+			if($interval->h == 1) {
+				$time_message = $interval->h . " hour ago";
+			}
+			else {
+				$time_message = $interval->h . " hours ago";
+			}
+		}
+		else if ($interval->i >= 1) {
+			if($interval->i == 1) {
+				$time_message = $interval->i . " minute ago";
+			}
+			else {
+				$time_message = $interval->i . " minutes ago";
+			}
+		}
+
+		else {
+			if($interval->s < 30) {
+				$time_message = " Just now";
+			}
+			else {
+				$time_message = $interval->s . " seconds ago";
+			} 
+		}
+
+		array_push($details_array, $sent_by);
+		array_push($details_array, $row['BODY']);
+		array_push($details_array, $time_message);
+
+		return $details_array;
+		
+		
+	}
+
+	public function getConvos() {
+		$userLoggedIn  = $this->user_obj->getUsername();
+		$return_string = "";
+		$convos = array();
+
+		$query = mysqli_query($this->con, "SELECT USER_TO,USER_FROM FROM MESSAGES WHERE USER_TO='$userLoggedIn' OR USER_FROM='$userLoggedIn' ORDER BY ID DESC");
+
+		while ($row= mysqli_fetch_array($query)) {
+			$user_to_push = ($row['USER_TO'] != $userLoggedIn) ? $row['USER_TO'] :$row['USER_FROM'];
+
+			if(!in_array($user_to_push, $convos)) {
+				array_push($convos, $user_to_push);
+			}
+		}
+
+		foreach($convos as $username) {
+			$user_found_obj = new User($this->con, $username);
+			$latest_message_details = $this->getLatestMessage($userLoggedIn, $username);
+
+			$dots = (strlen($latest_message_details[1]) >= 12) ? "..." : "";
+			$split = str_split($latest_message_details[1], 12);
+			$split = $split[0] . $dots;
+
+			$return_string .= "<a href='messages.php?u=$username'> <div class='user_found_messages'>
+								<img src='" . $user_found_obj->getProfilePic() . "' style='border-radius: 5px; margin-right: 5px;'>
+								" . $user_found_obj->getName() . "
+								<span class='timestamp_smaller' id='grey'> " . $latest_message_details[2] . "</span>
+								<p id='grey' style='margin: 0;'>" . $latest_message_details[0] . $split . "</p>
+								</div>
+								</a>";
+		}
+
+		return $return_string;
+
+	}
 }
 
 ?>
--- messages.php
+++ messages.php
@@ -43,7 +43,7 @@ if(isset($_POST['post_message'])) {
 	<?php  
 	if($user_to != "new") {
 		echo "<h4>You and <a href='$user_to'>" . $user_to_obj->getName() . "</a></h4><hr><br>";
-		echo "<div class='loaded_messages'>";
+		echo "<div class='loaded_messages' id='scroll_messages'>";
 			echo $message_obj->getMessages($user_to);
 		echo "</div>";
 	}
@@ -71,6 +71,21 @@ if(isset($_POST['post_message'])) {
 		
 	</div>
 
+	<script>
+		var div = document.getElementById("scroll_messages");
+		div.scrollTop = div.scrollHeight;
+	</script>
 
 
+</div>
+
+<div class="user_details column" id="conversations">
+	<h4>Conversations</h4>
+
+	<div class="loaded_conversations">
+		<?php echo $message_obj->getConvos(); ?>
+	</div>
+	<br>
+	<a href="messages.php?u=new">New Message</a>
+	
 </div>
